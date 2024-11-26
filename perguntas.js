ada5d5331d66125e539953d57f0c15111cca4a96
gabimarch5 <0000111246055xSP@al.educacao.sp.gov.br>
2024-11-26 16:00:17 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Genética',
+    'Onde os cromossomos homólogos ficam?',
+    'Os cromossomos homólogos podem ser encontrados nos núcleos de células somáticas e são compostos por DNA.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a definição de homologia na biologia?',
+    'Quando organismos de espécies diferentes apresentam órgãos que possuem a mesma origem embrionária e desempenham a mesma função ou não'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a definição de analogia na biologia?',
+    'O conceito de analogia se refere a organismos de diferentes espécies que possuem estruturas biológicas semelhantes, mas que não possuem uma mesma origem embrionária.'
+)
